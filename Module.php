c2487a2b5f0adb3a71e5a5df8e8325a4eb44cebd
Eddie Jaoude <eddie@jaoudestudios.com>
2014-01-10 06:16:20 +0000

    Removed config include

--- Module.php
+++ Module.php
@@ -59,11 +59,6 @@ class Module
         );
     }
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
     public function getAutoloaderConfig()
     {
         return array(
