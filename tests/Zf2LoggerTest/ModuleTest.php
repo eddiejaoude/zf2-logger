34a68318f2125c5e5311230d19a1d5eb1798dc6d
Eddie Jaoude <eddie@jaoudestudios.com>
2014-01-11 22:29:30 +0000

    Updated unit tests

--- tests/Zf2LoggerTest/ModuleTest.php
+++ tests/Zf2LoggerTest/ModuleTest.php
@@ -11,6 +11,9 @@ use EddieJaoude\Zf2Logger\Module;
 class ModuleTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var \EddieJaoude\Zf2Logger\Module
+     */
     private $instance;
 
     public function setUp()
@@ -21,5 +24,21 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
     public function testInstance()
     {
         $this->assertInstanceOf('\EddieJaoude\Zf2Logger\Module', $this->instance);
+    }
+
+    public function testGetAutoloaderConfig()
+    {
+        $response = $this->instance->getAutoloaderConfig();
+
+        $this->assertTrue(array_key_exists('EddieJaoude\Zf2Logger', $response['Zend\Loader\StandardAutoloader']['namespaces']));
+    }
+
+    public function testGetServiceConfig()
+    {
+        $response = $this->instance->getServiceConfig();
+
+        $this->assertTrue(array_key_exists('factories', $response));
+        $this->assertTrue(array_key_exists('EddieJaoude\Zf2Logger\Logger', $response['factories']));
+        $this->assertTrue(is_callable($response['factories']['EddieJaoude\Zf2Logger\Logger']));
     }
 }
