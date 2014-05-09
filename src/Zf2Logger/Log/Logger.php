e14eb84d70859eec53434e992cb81046f600831a
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2014-05-09 07:14:11 +0100

    Default 'extra' log information added

--- src/Zf2Logger/Log/Logger.php
+++ src/Zf2Logger/Log/Logger.php
@@ -2,13 +2,85 @@
 namespace EddieJaoude\Zf2Logger\Log;
 
 use Zend\Log\Logger as ZendLogger;
+use Zend\Authentication\AuthenticationService;
+use Zend\Http\PhpEnvironment\Request;
 
 /**
  * Class Logger
- *
- * @package Application\Event
+ * @package EddieJaoude\Zf2Logger\Log
  */
 class Logger extends ZendLogger
 {
+
+    /**
+     * @var AuthenticationService
+     */
+    private $authenticationService;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+
+    /**
+     * @param int   $priority
+     * @param mixed $message
+     * @param array $extra
+     *
+     * @return ZendLogger
+     */
+    final public function log($priority, $message, $extra = array())
+    {
+        $customExtra = array(
+            'Zf2Logger' => array(
+                'sessionId' => session_id(),
+                'host'      => $this->getRequest()->getUri()->getHost()
+            )
+        );
+
+        return parent::log($priority, $message, array_merge($extra, $customExtra));
+    }
+
+    /**
+     * @param AuthenticationService $authenticationService
+     *
+     * @return Logger
+     */
+    public function setAuthenticationService(AuthenticationService $authenticationService)
+    {
+        $this->authenticationService = $authenticationService;
+
+        return $this;
+    }
+
+    /**
+     * @return AuthenticationService
+     */
+    public function getAuthenticationService()
+    {
+        return $this->authenticationService;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Logger
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
+    /**
+     * @return Request Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
 
 }
---
+++ tests/Zf2LoggerTest/src/Log/LoggerTest.php
@@ -0,0 +1,115 @@
+<?php
+namespace EddieJaoude\Zf2Logger\Tests\Zf2LoggerTest\Log;
+
+use EddieJaoude\Zf2Logger\Log\Logger;
+
+/**
+ * Class LoggerTest
+ * @package EddieJaoude\Zf2Logger\Tests\Zf2LoggerTest\Log
+ */
+class LoggerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \EddieJaoude\Zf2Logger\Log\Logger
+     */
+    private $logger;
+
+    public function setUp()
+    {
+        $writer = new \Zend\Log\Writer\Mock;
+
+        $this->logger = new Logger();
+        $this->logger->addWriter($writer);
+
+        $authenticationService = \Mockery::mock('Zend\Authentication\AuthenticationService');
+        $this->logger->setAuthenticationService(
+            $authenticationService
+        );
+
+        $request = \Mockery::mock('Zend\Http\PhpEnvironment\Request');
+        $request->shouldReceive('getUri')->times(1)->andReturn(\Mockery::self());
+        $request->shouldReceive('getHost')->times(1)->andReturn('unit-test-host');
+        $this->logger->setRequest(
+            $request
+        );
+    }
+
+    public function testSetGetAuthenticationService()
+    {
+        $authenticationService = \Mockery::mock('Zend\Authentication\AuthenticationService');
+
+        $this->assertInstanceOf(
+            'EddieJaoude\Zf2Logger\Log\Logger',
+            $this->logger->setAuthenticationService($authenticationService)
+        );
+
+        $this->assertEquals(
+            $authenticationService,
+            $this->logger->getAuthenticationService()
+        );
+    }
+
+    public function testSetGetRequest()
+    {
+        $request = \Mockery::mock('Zend\Http\PhpEnvironment\Request');
+
+        $this->assertInstanceOf(
+            'EddieJaoude\Zf2Logger\Log\Logger',
+            $this->logger->setRequest($request)
+        );
+
+        $this->assertEquals(
+            $request,
+            $this->logger->getRequest()
+        );
+    }
+
+    public function testLogAndDefaultExtra()
+    {
+        $message = 'test message';
+
+        $this->assertInstanceOf(
+            'EddieJaoude\Zf2Logger\Log\Logger',
+            $this->logger->log(Logger::EMERG, $message)
+        );
+
+        $this->assertEquals(0, $this->logger->getWriters()->current()->events[0]['priority']);
+        $this->assertEquals('EMERG', $this->logger->getWriters()->current()->events[0]['priorityName']);
+        $this->assertEquals($message, $this->logger->getWriters()->current()->events[0]['message']);
+        $this->assertEquals(
+            array(
+                'Zf2Logger' => array(
+                    'sessionId' => '',
+                    'host' => 'unit-test-host'
+                )
+            ),
+            $this->logger->getWriters()->current()->events[0]['extra']
+        );
+    }
+
+    public function testExtraAddition()
+    {
+        $message = 'test message';
+        $extra = 'extra additional information for the logger';
+
+        $this->assertInstanceOf(
+            'EddieJaoude\Zf2Logger\Log\Logger',
+            $this->logger->log(Logger::DEBUG, $message, array($extra))
+        );
+
+        $this->assertEquals(7, $this->logger->getWriters()->current()->events[0]['priority']);
+        $this->assertEquals('DEBUG', $this->logger->getWriters()->current()->events[0]['priorityName']);
+        $this->assertEquals($message, $this->logger->getWriters()->current()->events[0]['message']);
+        $this->assertEquals(
+            array(
+                'Zf2Logger' => array(
+                    'sessionId' => '',
+                    'host' => 'unit-test-host'
+                ),
+                $extra
+            ),
+            $this->logger->getWriters()->current()->events[0]['extra']
+        );
+    }
+}
