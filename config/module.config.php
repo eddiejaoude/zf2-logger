6cf44f463f5641cdceac29af15cd209ee503dd8a
Merlin <romain.dary@eoko.fr>
2014-12-17 20:37:42 +0100

    KISS config (nothing inessential) and trailing comma

--- config/module.config.php
+++ config/module.config.php
@@ -2,13 +2,9 @@
 return array(
     'EddieJaoude\Zf2Logger' => array(
 
-        // will add the $logger object before the current PHP error handler
-        'registerErrorHandler'     => true, // errors logged to your writers
-        'registerExceptionHandler' => true, // exceptions logged to your writers
-
         // do not log binary responses
         // mime types reference http://www.sitepoint.com/web-foundations/mime-types-complete-list/
-        'doNotLog'                 => array(
+        'doNotLog' => array(
             'mediaTypes' => array(
                 'application/octet-stream',
                 'image/png',
@@ -16,5 +12,5 @@ return array(
                 'application/pdf'
             ),
         ),
-    )
+    ),
 );
